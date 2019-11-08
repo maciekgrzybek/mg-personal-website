9bf7d6a48f6f71a2cd28dba77f71f05fe0affb45
maciekgrzybek <maciekgrzybek1@gmail.com>
2019-11-08 07:37:24 +0000

    Header works

--- src/components/header.js
+++ src/components/header.js
@@ -3,33 +3,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import Logo from './Logo';
+import Icon from './Icon';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header>
+    <div>
       <Logo />
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    </div>
+    <div />
+    <div>
+      <Icon type="github" />
+      <Icon type="devto" />
     </div>
   </header>
 );
